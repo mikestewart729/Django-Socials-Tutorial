0e53caeb3c3c1cb23eaa579a71e8a2074c6367b3
Mike <mikestewart729@gmail.com>
2022-02-10 11:18:53 -0500

    Link profile_list and individual profiles

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,5 +1,6 @@
 # dwitter/views.py
 
+import re
 from django.shortcuts import render
 from .models import Profile
 
@@ -9,4 +10,8 @@ def dashboard(request):
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+    
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})
     
